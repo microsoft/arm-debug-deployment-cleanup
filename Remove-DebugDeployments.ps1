f146f134bc5b8d54b5dab4c64d9f314bfa91f265
Brian Moore <bmoore@microsoft.com>
2021-03-04 16:07:00 -0600

    Update Remove-DebugDeployments.ps1

--- Remove-DebugDeployments.ps1
+++ Remove-DebugDeployments.ps1
@@ -1,14 +1,15 @@
-## This script cleans up the deployments at management group level, subscription level, and resource group level with debugSettings set to 'RequestContent, ResponseContent'.
+## This script removes deployments at management group level, subscription level, and resource group level with debugSettings that contain 'RequestContent'.  Removing a deployment does NOT affect or remove any resources that were deployed during that deployment.
 ##
 ## Pre-requisites:
 ##        Windows users:
-##        Install Azure Az powershell module - Follow installation instructions on https://docs.microsoft.com/en-us/powershell/azure/install-az-ps?view=azps-5.6.0
+##        Install the Azure Az powershell module - Follow installation instructions on https://docs.microsoft.com/en-us/powershell/azure/install-az-ps
 ##
 ##        Linux users:
-##        Install Powershell for Linux (PSCore) - Follow instructions on https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-core-on-linux?view=powershell-7.1
-##        Install Azure Az powershell module - Follow installation instructions on https://docs.microsoft.com/en-us/powershell/azure/install-az-ps?view=azps-5.6.0
+##        Install Powershell for Linux (PSCore) - Follow instructions on https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-core-on-linux
+##        Install the Azure Az powershell module - Follow installation instructions on https://docs.microsoft.com/en-us/powershell/azure/install-az-ps
 ##
-## Usage: Use Connect-AzConnect to your respective tenant/cloud first (https://docs.microsoft.com/en-us/azure/azure-government/documentation-government-get-started-connect-with-ps).
+## Usage: Use Connect-AzConnect to your respective tenant/cloud first (https://docs.microsoft.com/en-us/azure/azure-government/documentation-government-get-started-connect-with-ps).  You must connect with an account that has permissions to delete the deployments you wish to remove.
+##
 ##        Then run this script via powershell.
 ##
 ##        Windows users: 
@@ -18,11 +19,10 @@
 ##        Linux users:
 ##        Open powershell with pwsh command on Terminal and execute the script
 ##        PS ./Remove-DebugDeployments.ps1
-## WARNING: As this script goes through all management groups, subscriptions and resource groups level deployments, the output can potentially be huge, thus we strongly recommend to redirect the output to a file
+##
 
-$ErrorActionPreference='Continue' # To make sure the script continues to run after failure
+$ErrorActionPreference='Continue' # To make sure the script continues to run after an individual failure
 
-## Assuming that user has already logged into the respective cloud/tenant.
 $managementGroups = Get-AzManagementGroup
 
 Write-Host "Iterating through the management groups..."
@@ -34,8 +34,8 @@ foreach ($managementGroup in $managementGroups) {
     $MGDeployments = Get-AzManagementGroupDeployment -ManagementGroupId $managementGroup.Name
 
     foreach ($MGDeployment in $MGDeployments) {
-        if ($MGDeployment.DeploymentDebugLogLevel -eq "RequestContent, ResponseContent") {
-            Write-Host "The Deployment - $($MGDeployment.DeploymentName) for the management group $($managementGroup.Name) has debugSettings enabled. Proceeding to delete this deployment..."
+        if ($MGDeployment.DeploymentDebugLogLevel -like "*RequestContent*") {
+            Write-Host "The Deployment - $($MGDeployment.DeploymentName) for the management group $($managementGroup.Name) contains RequestContent. Proceeding to delete this deployment..."
 
             Remove-AzManagementGroupDeployment -ManagementGroupId $managementGroup.Name -Name $MGDeployment.DeploymentName
 
@@ -54,16 +54,16 @@ $subscriptions = Get-AzSubscription
 Write-Host 'Iterating through the subscriptions...'
 
 ## Go through all the subscriptions. Look for deployments with debugSettings, and delete such deployments.
-## Go through all resource groups under the subscription. Look for deployments with debugSettings, and delete such deployments.
 foreach ($subscription in $subscriptions) {
+    Write-Host "Fetching deployments for subscription - $($subscription.Id)"
     Set-AzContext -Subscription $subscription.Id
-    $SubDeployments = Get-AzDeployment
+    $SubDeployments = Get-AzSubscriptionDeployment
 
     foreach ($SubDeployment in $SubDeployments) {
-        if ($SubDeployment.DeploymentDebugLogLevel -eq "RequestContent, ResponseContent") {
-            Write-Host "The Deployment - $($SubDeployment.DeploymentName) for the subscription $($subscription.Name) has debugSettings enabled. Proceeding to delete this deployment..."
+        if ($SubDeployment.DeploymentDebugLogLevel -like "*RequestContent*") {
+            Write-Host "The Deployment - $($SubDeployment.DeploymentName) for the subscription $($subscription.Name) contains RequestContent. Proceeding to delete this deployment..."
 
-            Remove-AzDeployment -Name $SubDeployment.DeploymentName
+            Remove-AzSubscriptionDeployment -Name $SubDeployment.DeploymentName
             
             if (-not $?) {
                 Write-Host "Failed to delete $($SubDeployment.DeploymentName)" -fore Red
@@ -74,18 +74,18 @@ foreach ($subscription in $subscriptions) {
         }
     }
 
-    Write-Host "Iterating through the resource groups under $($subscription.Name)..."
+    ## Go through all resource groups under the subscription. Look for deployments with debugSettings, and delete such deployments.
+    Write-Host "Iterating through the resource groups under $($subscription.id)..."
 
     $resourceGroups = Get-AzResourceGroup
-
     foreach ($resourceGroup in $resourceGroups) {
         Write-Host "Fetching deployments for resource group - $($resourceGroup.ResourceGroupName)"
 
         $RGDeployments = Get-AzResourceGroupDeployment -ResourceGroupName $resourceGroup.ResourceGroupName
 
         foreach ($RGDeployment in $RGDeployments) {
-            if ($RGDeployment.DeploymentDebugLogLevel -eq "RequestContent, ResponseContent") {
-                Write-Host "The Deployment - $($RGDeployment.DeploymentName) for the resource group $($resourceGroup.ResourceGroupName) has debugSettings enabled. Proceeding to delete this deployment..."
+            if ($RGDeployment.DeploymentDebugLogLevel -like "*RequestContent*") {
+                Write-Host "The Deployment - $($RGDeployment.DeploymentName) for the resource group $($resourceGroup.ResourceGroupName) contains RequestContent. Proceeding to delete this deployment..."
     
                 Remove-AzResourceGroupDeployment -ResourceGroupName $resourceGroup.ResourceGroupName -Name $RGDeployment.DeploymentName
                 if (-not $?) {
